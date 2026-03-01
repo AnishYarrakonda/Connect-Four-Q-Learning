c33f9fea285be9a2a594c38e9eb812a45adb2089
Anish Yarrakonda <number1noob1234@gmail.com>
2026-02-28 22:30:18 -0800

    sdfs

--- train.py
+++ train.py
@@ -4,4 +4,13 @@ import time
 from board import Board
 from agent import Agent
 
-def play_game(agent: Agent):
+# simulate a single game
+def play_game(agent: Agent, ):
+    board = Board()
+    done = False
+    winner = 0
+
+    while not done:
+        state = Board.board_to_tensor(board=board)
+
+        action = agent.
